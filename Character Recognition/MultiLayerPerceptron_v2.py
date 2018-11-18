d1bd5dfc2aab0440402e3619723f2d312848c128
higor21 <felype.cet.15@outlook.com>
2018-11-18 20:39:25 -0300

    version 2

--- Character Recognition/MultiLayerPerceptron_v2.py
+++ Character Recognition/MultiLayerPerceptron_v2.py
@@ -29,6 +29,32 @@ def load_data(path, cmd):
     training_data = zip(training_inputs, training_results)
     return training_data
 
+class Neuron():
+    """
+        Neurônio a ser instanciado 
+    """
+    def __init__(self, n_inputs = 0, threshold = 0, out = 0):
+        self.n_inputs = n_inputs
+        self.threshold = threshold
+        self.weights = np.random.randn(1,n_inputs)
+        self.output = out
+
+    def __str__(self):
+        return '[' + str(self.n_inputs) + ' , ' + str(self.threshold) + ' , ' + str(self.output) + ']'
+
+    def calculate_out(self, input_dt, func = "sigmoid"):
+        if(len(input_dt) == len(self.weights)):
+            self.output = self.act_function(np.dot(self.weights, input_dt) - self.threshold, func)
+        return self.output
+        
+    def act_function(self, value, nm_func):
+        """
+            seleciona qual a saída do neurônio de acordo com sua função de ativação
+        """
+        if nm_func == "sigmoid":
+            return 1.0/(1.0 + exp(-value))
+        
+
 class MultiLayersPerceptron():
     def __init__(self, layers, thresholds = True):
         """
@@ -37,16 +63,18 @@ class MultiLayersPerceptron():
             Multi-Layers Perceptron (MPL), respectivamente. 
             Além disso, ainda inicia aleatoriamente os pesos entre cada camada e os 'thresholds'(limites == biases)
             de cada neurônio ()
-        """
-        self.num_layers = len(layers)
+        """    
         self.size_layers = layers
+        self.neurons = []
+
         # define os limites de cada neurônio
-        self.biases = [(1.0 if thresholds else 0.0)*np.random.randn(i, 1) for i in self.size_layers[1:]]
-        # define as matrizes de pesos entre cada 
-        #print(self.biases)
-        self.weights = [np.random.randn(x,y) for y, x in zip(self.size_layers[:-1], self.size_layers[1:])]
-        print(len(self.weights))
-        #print(zip(self.biases, self.weights))
+        i = 0
+        while i < len(layers):
+            self.neurons.append([])
+            for k in range(layers[i]):
+                neu = Neuron(layers[i-1], np.random.uniform(0.0, 1.0)) if (i != 0) else Neuron(0,0)
+                self.neurons[i].append(neu)
+            i += 1
 
     def training(self, training_data, learning_rate):
         """
@@ -55,6 +83,8 @@ class MultiLayersPerceptron():
         """
         while(len(training_data)):
             output_data = self.calculate_output(training_data[-1][0])
+            print(output_data)
+            break
             error = self.calculate_error(training_data[-1][1], output_data)
             self.back_propagation(training_data[-1][0], output_data, error, learning_rate)
             training_data.pop()
@@ -71,23 +101,28 @@ class MultiLayersPerceptron():
         """
             Calcula a saída da rede neural para uma dada entrada. Além disso,
         """
-        for b, w in zip(self.biases, self.weights):
-            # o resultado de 'np.dot(w,input_data) - b)' acaba sendo um 
-            # 'array()' q será calculado pela função 'act_funciton()'
-            for i in range(len(w)):
-                input_data[i] = self.act_function(np.dot(w[i],input_data) - b[i], "sigmoid")        
-            input_data = input_data[:i+1]
-        return input_data
+
+        self.neurons[0] = [Neuron(out = x) for x in input_data]
+        listOuts = self.listOutputs(self.neurons[0])
+        
+        for x in self.neurons[1:]:
+            for i in x:
+                i.calculate_out(listOuts, "sigmoid")
+            listOuts = self.listOutputs(x)
+
+        return listOuts
+
+    def listOutputs(self, neus):
+        print(type(neus[0].output))
+        out = []
+
+        for x in neus:
+            out.append(list(x.output) if type(neus[0].output) != 'int' else x.output)
+        return out
 
     def calculate_error(self, expected_output, real_output):
         return expected_output - real_output 
 
-    def act_function(self, value, nm_func = "sigmoid"):
-        """
-            seleciona qual a saída do neurônio de acordo com sua função de ativação
-        """
-        if nm_func == "sigmoid":
-            return 1.0/(1.0 + exp(-value))
 
 
 # 'tr_d' são todas as imagens que serão usadas para treinar a rede
@@ -96,6 +131,7 @@ tr_d = load_data('./mnist.pkl.gz', 'rb')
 # Criando uma rede cuja a entrada será uma imagem de 28x28 = 784 pixels
 # e a saída será um vetor de 10 posições 
 network_mlp = MultiLayersPerceptron([784,30,30,10])
+#network_mlp = MultiLayersPerceptron([2,3,3,2])
 
 # Treinando a rede com o banco de imagens 'tr_d' e a uma taxa de aprendizado de 0.3
 network_mlp.training(tr_d, 0.3)
