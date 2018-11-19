e0d52b98dbb5974d03fc1332efc8ae2e1c37d2a8
higor21 <felype.cet.15@outlook.com>
2018-11-18 22:16:15 -0300

    new update

--- Character Recognition/MultiLayerPerceptron.py
+++ Character Recognition/MultiLayerPerceptron.py
@@ -8,86 +8,104 @@ import gzip
 
 
 def vectorize(x):
-    """
-        cria matris 10x1 e coloca o valor 1 na posição 'x'
-    """
-    e = np.zeros((10, 1))
-    e[x] = 1.0
-    return e
+	"""
+		cria matris 10x1 e coloca o valor 1 na posição 'x'
+	"""
+	e = np.zeros((10, 1))
+	e[x] = 1.0
+	return e
 
 def load_data(path, cmd):
-    """
-        Abre o arquivo no caminho 'path' e retorna os dados das
-        imagens que serão utilizados para o treinamento da rede neural
-    """
-    file = gzip.open(path, cmd)
-    data = cPickle.load(file)[0]
-    file.close()
+	"""
+		Abre o arquivo no caminho 'path' e retorna os dados das
+		imagens que serão utilizados para o treinamento da rede neural
+	"""
+	file = gzip.open(path, cmd)
+	data = cPickle.load(file)[0]
+	file.close()
 
-    training_inputs = [np.reshape(x, (784, 1)) for x in data[0]]
-    training_results = [vectorize(y) for y in data[1]]
-    training_data = zip(training_inputs, training_results)
-    return training_data
+	training_inputs = [np.reshape(x, (784, 1)) for x in data[0]]
+	training_results = [vectorize(y) for y in data[1]]
+	training_data = zip(training_inputs, training_results)
+	return training_data
+
+
+def arrayToList(vect):
+	"""
+		Função usada para converter um vetor do tipo 'nparray' para o tipo 'list'
+	"""
+	if type(vect).__module__ == np.__name__:
+		vect = vect.tolist()
+	for i in range(len(vect)):
+		if type(vect[i]).__module__ == np.__name__:
+			vect[i] = vect[i].tolist()
+	return vect
 
 class MultiLayersPerceptron():
-    def __init__(self, layers, thresholds = True):
-        """
-            Inicializa a rede neural com várias camadas (layers) de distintos tamanhos. 
-            Dentre essas camandas, a primeira e a última representa a entrada e saida da
-            Multi-Layers Perceptron (MPL), respectivamente. 
-            Além disso, ainda inicia aleatoriamente os pesos entre cada camada e os 'thresholds'(limites == biases)
-            de cada neurônio ()
-        """
-        self.num_layers = len(layers)
-        self.size_layers = layers
-        # define os limites de cada neurônio
-        self.biases = [(1.0 if thresholds else 0.0)*np.random.randn(i, 1) for i in self.size_layers[1:]]
-        # define as matrizes de pesos entre cada 
-        #print(self.biases)
-        self.weights = [np.random.randn(x,y) for y, x in zip(self.size_layers[:-1], self.size_layers[1:])]
-        print(len(self.weights))
-        #print(zip(self.biases, self.weights))
-
-    def training(self, training_data, learning_rate):
-        """
-            Método para treinamento da rede neural com base nos dados 'training_data' e na
-            taxa de aprendizado 'learning_rate'
-        """
-        while(len(training_data)):
-            output_data = self.calculate_output(training_data[-1][0])
-            error = self.calculate_error(training_data[-1][1], output_data)
-            self.back_propagation(training_data[-1][0], output_data, error, learning_rate)
-            training_data.pop()
-            break
+	def __init__(self, layers, thresholds = True):
+		"""
+			Inicializa a rede neural com várias camadas (layers) de distintos tamanhos. 
+			Dentre essas camandas, a primeira e a última representa a entrada e saida da
+			Multi-Layers Perceptron (MPL), respectivamente. 
+			Além disso, ainda inicia aleatoriamente os pesos entre cada camada e os 'thresholds'(limites == biases)
+			de cada neurônio ()
+		"""
+		self.num_layers = len(layers)
+		self.size_layers = layers
+		# define os limites de cada neurônio
+		self.biases = arrayToList([(1.0 if thresholds else 0.0)*np.random.randn(i, 1) for i in self.size_layers[1:]])
+		# define as matrizes de pesos entre cada 
+		#print(self.biases)
+		self.weights = arrayToList([np.random.randn(x,y) for y, x in zip(self.size_layers[:-1], self.size_layers[1:])])
+		print(self.weights)
 
 
-    def back_propagation(self, input_data, real_output, error, learning_rate):
-        delta = real_output*(1 - real_output)*error
-        for b, w in zip(self.biases, self.weights):
-            print(str(b) + ' <-> ' + str(w))
-            return
 
-    def calculate_output(self, input_data):
-        """
-            Calcula a saída da rede neural para uma dada entrada. Além disso,
-        """
-        for b, w in zip(self.biases, self.weights):
-            # o resultado de 'np.dot(w,input_data) - b)' acaba sendo um 
-            # 'array()' q será calculado pela função 'act_funciton()'
-            for i in range(len(w)):
-                input_data[i] = self.act_function(np.dot(w[i],input_data) - b[i], "sigmoid")        
-            input_data = input_data[:i+1]
-        return input_data
 
-    def calculate_error(self, expected_output, real_output):
-        return expected_output - real_output 
+	def training(self, training_data, learning_rate):
+		"""
+			Método para treinamento da rede neural com base nos dados 'training_data' e na
+			taxa de aprendizado 'learning_rate'
+		"""
+		while(len(training_data)):
+			output_data, list_outs = self.calculate_output(training_data[-1][0])
+			error = self.calculate_error(training_data[-1][1], output_data)
+			break
+			self.back_propagation(training_data[-1][0], output_data, error, list_outs, learning_rate)
+			training_data.pop()
+			break
 
-    def act_function(self, value, nm_func = "sigmoid"):
-        """
-            seleciona qual a saída do neurônio de acordo com sua função de ativação
-        """
-        if nm_func == "sigmoid":
-            return 1.0/(1.0 + exp(-value))
+
+	def back_propagation(self, input_data, real_output, error, list_outs, learning_rate = 0.5):
+		delta = real_output*(1 - real_output)*error
+		for b, w in zip(self.biases, self.weights):
+			print(str(b) + ' <-> ' + str(w))
+			for i in range(len(w)):
+				pass
+
+	def calculate_output(self, input_data):
+		"""
+			Calcula a saída da rede neural para uma dada entrada. Além disso,
+		"""
+		list_outs = []
+		for b, w in zip(self.biases, self.weights):
+			# o resultado de 'np.dot(w,input_data) - b)' acaba sendo um 
+			# 'array()' q será calculado pela função 'act_funciton()'
+			for i in range(len(w)):
+				input_data[i] = self.act_function(np.dot(w[i],input_data) - b[i], "sigmoid")        
+			input_data = input_data[:i+1]
+			list_outs.append(input_data)
+		return (input_data, arrayToList(list_outs))
+
+	def calculate_error(self, expected_output, real_output):
+		return expected_output - real_output 
+
+	def act_function(self, value, nm_func = "sigmoid"):
+		"""
+			seleciona qual a saída do neurônio de acordo com sua função de ativação
+		"""
+		if nm_func == "sigmoid":
+			return 1.0/(1.0 + exp(-value))
 
 
 # 'tr_d' são todas as imagens que serão usadas para treinar a rede
